fc9087eb36485eb470028e3925fa07f54df316cd
Lucas Ferreira <casferreiraa@gmail.com>
2023-09-13 23:56:09 -0300

    feat(server):add context, conexao mysql e insercao bd

--- server/main.go
+++ server/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-type Cotacao struct {
+type Dolar struct {
+	ID    int `gorm:"primaryKey"`
+	Valor string
+	gorm.Model
+}
+
+type CotacaoDolar struct {
 	USDBRL `json:"USDBRL"`
 }
 
@@ -26,28 +38,69 @@ type USDBRL struct {
 	CreateDate string `json:"create_date"`
 }
 
+var db *gorm.DB
+
 func main() {
-	var apiUrl = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
-
-	print(apiUrl)
-	print("\n")
-
-	req, err := http.Get(apiUrl)
+	strConn := "root:root@tcp(localhost:3306)/cotacao?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	db, err = gorm.Open(mysql.Open(strConn), &gorm.Config{})
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao fazer requisição %v\n", err)
+		fmt.Fprintf(os.Stderr, "Erro ao iniciar conexão com banco de dados %v\n", err)
 	}
-	defer req.Body.Close()
+	db.AutoMigrate(&Dolar{})
 
-	res, err := io.ReadAll(req.Body)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao ler resposta %v\n", err)
-	}
+	http.HandleFunc("/cotacao", func(w http.ResponseWriter, r *http.Request) {
 
-	var cotacao Cotacao
-	err = json.Unmarshal(res, &cotacao)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao fazer parse da informação %v\n", err)
+		ctx := r.Context()
+		defer log.Println("fim request")
+		var apiUrl = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+
+		select {
+		case <-time.After(200 * time.Millisecond):
+
+			// req, err := http.NewRequestWithContext(ctx, "GET", apiUrl, nil)
+			req, err := http.Get(apiUrl)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Erro ao fazer requisição %v\n", err)
+			}
+			defer req.Body.Close()
+
+			res, err := io.ReadAll(req.Body)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Erro ao ler resposta %v\n", err)
+			}
+
+			var cotacao CotacaoDolar
+			err = json.Unmarshal(res, &cotacao)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Erro ao fazer parse da informação %v\n", err)
+			}
+			fmt.Println(cotacao)
+
+			log.Println("Request processada com sucesso")
+			registrarCotacao(ctx, cotacao.Bid)
+			json.NewEncoder(w).Encode(cotacao)
+
+		case <-ctx.Done():
+			log.Println("Request cancelada")
+		}
+
+	})
+	http.ListenAndServe("127.0.0.1:8080", nil)
+
+}
+
+func registrarCotacao(ctx context.Context, valor string) {
+
+	select {
+	case <-time.After(10 * time.Millisecond):
+		db.Create(&Dolar{
+			Valor: valor,
+		})
+		fmt.Println(valor)
+		log.Println("Valor registrado com sucesso")
+	case <-ctx.Done():
+		log.Println("Erro ao registrar informação no banco de dados")
 	}
-	fmt.Println(cotacao)
 
 }
